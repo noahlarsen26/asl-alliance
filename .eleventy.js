f9f72f7b8ca2883989b053010b51d7344aa4cefa
Noah Larsen <noahlarsen24@gmail.com>
2022-10-27 15:11:08 -0500

    change date format

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
 // prettier-ignore
-const { DateTime } = require("luxon");
+// const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/styles.css");
   eleventyConfig.addPassthroughCopy("./images");
   eleventyConfig.addPassthroughCopy("./src/admin");
   eleventyConfig.addPassthroughCopy("./src/scripts");
-  eleventyConfig.addFilter("postDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.Date_MED);
-  });
+  // eleventyConfig.addFilter("postDate", (dateObj) => {
+  //   return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.Date_MED);
+  // });
 
   return {
     dir: {
